f564c0ee93b658fc06e1517e58490484d2faf703
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-08-25 21:03:37 -0400

    descs en place

--- index.php
+++ index.php
@@ -808,11 +808,20 @@
                         <div class="panel-group resume" id="work">
                             <?php
 							$i = 0; //$i va être le numéro de "work" pour les accordéons de travail
+                            $a = 0; //$a va être pour la liste de déscriptions
                             $requetePositionsWeb = "SELECT company, jobtitle, start_date, end_date FROM positions WHERE language='fr-CA' AND type = 'WEB' ORDER BY start_date DESC;";
                             $resultatPositionsWeb = $lien->query($requetePositionsWeb);
                             while($rang_positionsweb = $resultatPositionsWeb->fetch_assoc())
                             {
 							$i++; //Ceci ajoute 1 au numéro de "work" pour les accordéons de travail
+                            $a++; //Ceci ajoute 1 pour changer la gamme de déscriptions
+                            if ($a == 1) {
+                                $descriptionsWeb = 'apsa';
+                            } elseif ($a == 2) {
+                                $descriptionsWeb = 'georgetown';
+                            } else if ($a ==3) {
+                                $descriptionsWeb = 'iq';
+                            }
                             echo "<div class='resume-item'>";
                                 echo "<div class='resume-year'>";
                                     if (is_null($rang_positionsweb['end_date'])) {
@@ -840,10 +849,17 @@
                                     echo "<div id='work{$i}' class='panel-collapse collapse in'>";
                                         echo "<div class='panel-body text-grey'>";
                                             echo "<p>
-                                                Responsibilities:
-                                                <br>
-                                                Design of print collateral and websites for various clients and markets, development of new and existing brands, 3D modeling of floor and site plans.
-                                            </p>";
+                                                R&eacute;sponsibilities:
+                                                <br>";
+                                                $requeteDesc = "SELECT job FROM descriptions WHERE language='fr-CA' AND type = 'WEB' AND company = '".$descriptionsWeb."' ORDER BY id;";
+                                                $resultatDesc = $lien->query($requeteDesc);
+                                                echo "<ul>";
+                                                while($rang_desc = $resultatDesc->fetch_assoc())
+                                                {
+                                                    echo "<li>{$rang_desc['job']}</li>";
+                                                }
+                                                echo "</ul>";
+                                            echo "</p>";
                                         echo "</div>";
                                         echo "<!-- //.panel-body -->";
                                     echo "</div>";
