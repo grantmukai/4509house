154d5608536fa19840e97d132784d969b2adcd5f
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-09-13 19:15:14 -0400

    sections

---
+++ sections/contact.php
@@ -0,0 +1,49 @@
+<!-- DÉBUT SECTION CONTACT -->
+<section id="contact" class="section">
+    <div class="container section-wrapper">
+        <div class="section-content">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2 class="section-title">Contactez-Moi</h2>
+
+                    <p class="section-subtitle">
+                        Avez-vous un projet que vous aimeriez discuter?
+                    </p>
+
+                    <span class="divider center"></span>
+                </div>
+                <!-- //.col-md-12 -->
+            </div>
+            <!-- //.row -->
+            
+            <div class="row">
+                <div class="col-md-12">
+
+                    <!-- DÉBUT DÉTAILS CONTACT -->
+                    <div class="contact-details text-center">
+                        <div class="speech-bubble">
+                            <i class="icon-Speach-Bubble11"></i>
+
+                            <h4 style="font-size:24px;" class="text-grey">
+                                Dites<br>Bonjour
+                            </h4>
+                        </div>
+                        <!-- //.speech-bubble -->
+
+                        <h2>Grant <strong>Muka&iuml;</strong></h2>
+
+                        <h4 class="text-grey">hello@grantmukai.com</h4>
+
+                        <span class="divider center"></span>
+                    </div>
+                    <!-- //FIN DÉTAILS CONTACT -->
+                </div>
+                <!-- //.col-md-12 -->
+            </div>
+            <!-- //.row -->
+        </div>
+        <!-- //.section-content -->
+    </div>
+    <!-- //.container -->
+</section>
+<!-- //FIN SECTION CONTACT -->
