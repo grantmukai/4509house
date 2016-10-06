bdba1330557cbd016bc8267157d805068d90b3e5
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-10-05 20:02:51 -0400

    fr-FR ajouté

---
+++ sections/fr-FR/hero.php
@@ -0,0 +1,24 @@
+<!-- DÉBUT HÉRO -->
+<header id="home" class="hero">
+    <div class="hero-body">
+        <div class="hero-text text-center">
+            <h1>
+                Bonjour, je m'appelle Kiyoshi MUKA&Iuml; <span class="blinker"></span>
+            </h1>
+            
+            <!-- <h1 class="small">D&eacute;veloppeur Web</h1> -->
+            
+            <p>D&eacute;veloppement Web | E-mails de marketing | Administration de Don&eacute;es | Services d&apos;Adh&eacute;sion</p>
+            
+            <div class="page-scroll">
+                <a href="#profile" class="btn btn-lg btn-custom">
+                    Apprenez d'avantage :)
+                </a>
+            </div>
+            <!-- //.page-scroll -->
+        </div>
+        <!-- //.hero-text -->
+    </div>
+    <!-- //.hero-body -->
+</header>
+<!-- //FIN HÉRO -->
---
+++ sections/fr-FR/tete.php
@@ -0,0 +1,12 @@
+<?php date_default_timezone_set("America/Montreal");
+/*Pour France SEULEMENT!!*/
+$jeSuisEnEurope = "oui";
+$imageVille = 1;
+$villeMax = 2;
+$drapeauNational = 1;
+/*Fin code spécifique au Québec */
+if (isset($imageVille)) {
+    $quelleVille = mt_rand(0,$villeMax);
+    include "assets/images/villes/fr-CA/credits.php";
+}
+?>
