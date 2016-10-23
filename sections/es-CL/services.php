fc82f51851608137ed7b1e4df7c3c1859d029527
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-10-23 16:02:56 -0400

    Chile

---
+++ sections/es-CL/services.php
@@ -0,0 +1,114 @@
+<!-- DÉBUT SECTION SERVICES -->
+<section id="services" class="section">
+    <div class="container section-wrapper">
+        <div class="section-content">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center">
+                    <h2 class="section-title">Lo Que Puedo Hacer</h2>
+
+                    <p class="section-subtitle">
+                        Me encanta lo que hago. Estoy orgulloso de lo que hago. Y no puedo hacer casi la mitad, tres cuartos, nueve décimas partes de algo. Si voy a embarcarse en un proyecto, que estoy pasando.
+                    </p>
+
+                    <span class="divider center"></span>
+                </div>
+                <!-- //.col-md-8 -->
+            </div>
+            <!-- //.row -->
+        
+            <!-- DÉBUT SERVICES -->
+            <div class="services">
+                <div class="row">
+                    <div class="col-sm-4 col-md-4 item top dark-bg match-height">
+                        <div class="inner-content">
+                            <i class="icon-Pen colored-text"></i>
+
+                            <h4>1. Escritura</h4>
+
+                            <p class="text-grey">
+                                Porque estudi&eacute; el periodismo, yo se como bien escribir para las publicaciones escritas y electr&oacute;nicas y utilic&eacute; mis competencias para reportar las noticias y tambi&eacute;n para acentuar las empresas peque&ntilde;as en el espacio de promoci&oacute;n.
+                            </p>
+                        </div>
+                        <!-- //.inner-content -->
+                    </div>
+                    <!-- //.item -->
+
+                    <div class="col-sm-4 col-md-4 item top match-height">
+                        <div class="inner-content">
+                            <i class="icon-Film-Board colored-text"></i>
+
+                            <h4>2. Multimedia</h4>
+
+                            <p class="text-grey">
+                                En mi otra vida, trabaj&eacute; como periodista en la televisi&oacute;n y editor de v&iacute;deos. Realic&eacute; mis propios reportajes del inicio hasta la fin y produj&eacute; mis propios efectos especiales.
+                            </p>
+                        </div>
+                        <!-- //.inner-content -->
+                    </div>
+                    <!-- //.item -->
+
+                    <div class="col-sm-4 col-md-4 item top dark-bg match-height">
+                        <div class="inner-content">
+                            <i class=" icon-Email colored-text"></i>
+
+                            <h4>3. Dise&ntilde;o Correo Electr&oacute;nico</h4>
+
+                            <p class="text-grey">
+                                Compreniendo que el dise&ntilde;o de correo electr&oacute;nico no es el mismo que el dise&ntilde;o Web, soy competente con el c&oacute;digo &ldquo;obsoleto&rdquo; del HTML y CSS. Asimismo, tengo una buena idea de lo que Outlook acepta y no acepta.
+                            </p>
+                        </div>
+                        <!-- //.inner-content -->
+                    </div>
+                    <!-- //.item -->
+                    
+                    <div class="col-sm-4 col-md-4 item match-height">
+                        <div class="inner-content">
+                            <i class="icon-Code-Window colored-text"></i>
+
+                            <h4>4. C&oacute;digo Limpio</h4>
+
+                            <p class="text-grey">
+                                Estoy serio cuando escribo c&oacute;digo HTML5, CSS3 y PHP7 el m&aacute;s limpio y actualizado. Mi c&oacute;digo es correctamente sangrado y sigue los est&aacute;ndares establicidos por el W3C.
+                            </p>
+                        </div>
+                        <!-- //.inner-content -->
+                    </div>
+                    <!-- //.item -->
+
+                    <div class="col-sm-4 col-md-4 item dark-bg match-height">
+                        <div class="inner-content">
+                            <i class="icon-Cursor-Click2 colored-text"></i>
+
+                            <h4>5. Dise&ntilde;o Web Adaptable</h4>
+
+                            <p class="text-grey">
+                                No solo comprendo la implementaci&oacute;n de <em>media quieres</em> para flotar y escalar contenidos para el dise&ntilde;o Web adaptable, comprendo tambi&eacute;n las mejores pr&aacute;cticas para crear un correo electr&oacute;nico adaptable (cuando apoyado).
+                            </p>
+                        </div>
+                        <!-- //.inner-content -->
+                    </div>
+                    <!-- //.item -->
+
+                    <div class="col-sm-4 col-md-4 item match-height">
+                        <div class="inner-content">
+                            <i class="icon-First-Aid colored-text"></i>
+
+                            <h4>6. Apoyo</h4>
+
+                            <p class="text-grey">
+                                Aunque proporciono servicios Web para mi asociaci&oacute;n, ofrezco tambi&eacute;n entrenamiento a nuestros empleados sobre c&oacute;mo pueden administrar sus propios contenidos y ayudo a nuestros miembros en ingl&eacute;s, franc&eacute;s y espa&ntilde;ol con preguntas de membres&iacute;a.
+                            </p>
+                        </div>
+                        <!-- //.inner-content -->
+                    </div>
+                    <!-- //.item -->
+                </div>
+                <!-- //.row -->
+            </div>
+            <!-- //FIN SERVICES -->
+        </div>
+        <!-- //.section-content -->
+    </div>
+    <!-- //.container -->
+</section>
+<!-- //FIN SECTION SERVICES -->
---
+++ sections/es-CL/tete.php
@@ -0,0 +1,10 @@
+<?php date_default_timezone_set("America/Mexico_City");
+/*Para Chile SOLAMENTE!!*/
+//$imageVille = 1;
+//$villeMax = 3;
+/*Fin código específico para Chile */
+//if (isset($imageVille)) {
+//    $quelleVille = mt_rand(0,$villeMax);
+//    include "assets/images/villes/es-MX/credits.php";
+//}
+?>
