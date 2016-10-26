5b8c71592698381b82796b3c67e1a63c3b9c5106
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-10-26 19:28:15 -0400

    archivos para uruguay

---
+++ sections/es-UY/tete.php
@@ -0,0 +1,10 @@
+<?php date_default_timezone_set("America/Montevideo");
+/*Para Uruguay SOLAMENTE!!*/
+//$imageVille = 0;
+//$villeMax = 0;
+/*Fin código específico para Uruguay */
+//if (isset($imageVille)) {
+//    $quelleVille = mt_rand(0,$villeMax);
+//    include "assets/images/villes/es-UY/credits.php";
+//}
+?>
