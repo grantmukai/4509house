e0ffc5f4e81dc9fe27d5e7c1dc779bc4685325af
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-11-14 19:57:54 -0500

    puerto rico sigs

--- sections/es-PR/profil.php
+++ sections/es-PR/profil.php
@@ -55,7 +55,7 @@
                                 </p>
 								
                                 <p class="text-grey">
-                                    En mi tiempo libre, me gustan mucho los bailes lindy hop y blues y me interesan las otras culturas internacionales de Washington.
+                                    En mi tiempo libre, me gustan mucho los bailes Lindy Hop y Blues y me interesan las otras culturas internacionales de Washington.
                                 </p>
 								
                                 <p class="text-grey">
