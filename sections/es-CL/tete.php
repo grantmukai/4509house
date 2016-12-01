579639bb4491757cf9941f2e7803857885b7a382
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-11-30 19:26:37 -0500

    Fotos chilenos

--- sections/es-CL/tete.php
+++ sections/es-CL/tete.php
@@ -1,10 +1,10 @@
 <?php date_default_timezone_set("America/Mexico_City");
 /*Para Chile SOLAMENTE!!*/
-//$imageVille = 1;
-//$villeMax = 3;
+$imageVille = 1;
+$villeMax = 0;
 /*Fin código específico para Chile */
-//if (isset($imageVille)) {
-//    $quelleVille = mt_rand(0,$villeMax);
-//    include "assets/images/villes/es-MX/credits.php";
-//}
+if (isset($imageVille)) {
+    $quelleVille = mt_rand(0,$villeMax);
+    include "assets/images/villes/es-CL/credits.php";
+}
 ?>
