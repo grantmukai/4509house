e0ca2532734664184200ff26d0ac1e6b3d4b1d5c
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-12-04 14:07:00 -0500

    Argentina

---
+++ sections/es-AR/tete.php
@@ -0,0 +1,10 @@
+<?php date_default_timezone_set("America/Buenos_Aires");
+/*Para Argentina SOLAMENTE!!*/
+//$imageVille = 0;
+//$villeMax = 0;
+/*Fin código específico para Argentina */
+//if (isset($imageVille)) {
+//    $quelleVille = mt_rand(0,$villeMax);
+//    include "assets/images/villes/es-AR/credits.php";
+//}
+?>
