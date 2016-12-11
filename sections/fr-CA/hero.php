d80f5f52b230ca12a559d650cc3bad5a795e34a9
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-12-11 15:20:48 -0500

    ajout de plus WEB et COM

--- sections/fr-CA/hero.php
+++ sections/fr-CA/hero.php
@@ -5,15 +5,33 @@
             <h1>
                 Bonjour, mon nom est Grant Muka&iuml; <span class="blinker"></span>
             </h1>
-            
-            <!-- <h1 class="small">D&eacute;veloppeur Web</h1> -->
-            
-            <p>D&eacute;veloppement Web | Courriels de Marketing | Administration des Donn&eacute;es | Services d’Adh&eacute;sion</p>
-            
+
+            <?php
+            if ($metier_actuel == "WEB") {
+                echo "<p>D&eacute;veloppement Web et Gestion de Bases de Donn&eacute;es</p>";
+            }
+            if ($metier_actuel == "COM") {
+                echo "<p>Communications et Relations Publiques</p>";
+            }
+            ?>
+                                    
             <div class="page-scroll">
                 <a href="#profil" class="btn btn-lg btn-custom">
                     Apprenez d'avantage :)
                 </a>
+                <br />
+                <?php
+                if ($metier_actuel == "WEB") {
+                echo "<a href='http://www.grantmukai.com?metier=com' class='btn btn-xs btn-custom' style='margin-top:10px;'>
+                Vous cherchez mon expe&eacute;rience en communicacations? &raquo;
+                </a>";
+                }
+                if ($metier_actuel == "COM") {
+                echo "<a href='http://www.grantmukai.com?metier=web' class='btn btn-xs btn-custom' style='margin-top:10px;'>
+                Vous cherchez mon expe&eacute;rience en d&eacute;veloppement Web? &raquo;
+                </a>";  
+                }
+                ?>
             </div>
             <!-- //.page-scroll -->
         </div>
