e96a755942fbd5e7901902a116ac16299e68761e
Kiyoshi Mukai <hello@kiyoshimukai.com>
2017-01-04 19:24:46 -0500

    Uruguay fotos

--- sections/es-UY/tete.php
+++ sections/es-UY/tete.php
@@ -1,10 +1,10 @@
 <?php date_default_timezone_set("America/Montevideo");
 /*Para Uruguay SOLAMENTE!!*/
-//$imageVille = 0;
-//$villeMax = 0;
+$imageVille = 0;
+$villeMax = 0;
 /*Fin código específico para Uruguay */
-//if (isset($imageVille)) {
-//    $quelleVille = mt_rand(0,$villeMax);
-//    include "assets/images/villes/es-UY/credits.php";
-//}
+if (isset($imageVille)) {
+    $quelleVille = mt_rand(0,$villeMax);
+    include "assets/images/villes/es-UY/credits.php";
+}
 ?>
