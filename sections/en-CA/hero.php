7609628fac1124a27ffd6ccf42ab3b45eaddacff
grantmukai <grantmukai@users.noreply.github.com>
2017-01-13 15:53:01 -0500

    canada english buttons head

--- sections/en-CA/hero.php
+++ sections/en-CA/hero.php
@@ -6,33 +6,7 @@
                 Hello, I'm Grant Mukai <span class="blinker"></span>
             </h1>
             
-            <?php
-            if ($metier_actuel == "WEB") {
-                echo "<p>Web Development &amp; Database Management</p>";
-            }
-            if ($metier_actuel == "COM") {
-                echo "<p>Communications and Public Relations</p>";
-            }
-            ?>
-            
-            <div class="page-scroll">
-                <a href="#profile" class="btn btn-lg btn-custom">
-                    Learn More :)
-                </a>
-                <br />
-                <?php
-                if ($metier_actuel == "WEB") {
-                echo "<a href='http://www.grantmukai.com?metier=com' class='btn btn-xs btn-custom' style='margin-top:10px;'>
-                Are you looking for my communications work? &raquo;
-                </a>";
-                }
-                if ($metier_actuel == "COM") {
-                echo "<a href='http://www.grantmukai.com?metier=web' class='btn btn-xs btn-custom' style='margin-top:10px;'>
-                Are you looking for my Web development work? &raquo;
-                </a>";  
-                }
-                ?>
-            </div>
+            <?php include 'titre_en.html'; ?>
             <!-- //.page-scroll -->
         </div>
         <!-- //.hero-text -->
