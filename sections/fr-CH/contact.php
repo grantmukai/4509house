e8e0607d604f6443ba0a2e94e7ce35eaa9e9eaf8
Kiyoshi Mukai <hello@kiyoshimukai.com>
2017-01-24 20:46:43 -0500

    Suisse mise à jour web

--- sections/fr-CH/contact.php
+++ sections/fr-CH/contact.php
@@ -30,9 +30,9 @@
                         </div>
                         <!-- //.speech-bubble -->
 
-                        <h2>Kiyoshi <strong>MUKA&Iuml;</strong></h2>
+                        <h2>Grant <strong>MUKA&Iuml;</strong></h2>
 
-                        <h4 class="text-grey">bonjour@kiyoshimukai.com</h4>
+                        <h4 class="text-grey">bonjour@grantmukai.com</h4>
 
                         <span class="divider center"></span>
                     </div>
--- sections/fr-CH/hero.php
+++ sections/fr-CH/hero.php
@@ -3,38 +3,12 @@
     <div class="hero-body">
         <div class="hero-text text-center">
             <h1>
-                Bonjour! Je m'appelle Kiyoshi MUKA&Iuml;<br />Guten Tag, Buongiorno, Allegra <span class="blinker"></span>
+                Bonjour! Je m'appelle Grant MUKA&Iuml;<br />Guten Tag, Buongiorno, Allegra <span class="blinker"></span>
             </h1>
             
-            <?php
-            if ($metier_actuel == "WEB") {
-                echo "<p>D&eacute;veloppement Web et Gestion de Bases de Donn&eacute;es</p>";
-            }
-            if ($metier_actuel == "COM") {
-                echo "<p>Communications et Relations Publiques</p>";
-            }
-            ?>
-                                    
-            <div class="page-scroll">
-                <a href="#profile" class="btn btn-lg btn-custom">
-                    Apprenez d'avantage :)
-                </a>
-                <br />
-                <?php
-                if ($metier_actuel == "WEB") {
-                echo "<a href='http://www.grantmukai.com?metier=com' class='btn btn-xs btn-custom' style='margin-top:10px;'>
-                Vous cherchez mon expe&eacute;rience en communicacations? &raquo;
-                </a>";
-                }
-                if ($metier_actuel == "COM") {
-                echo "<a href='http://www.grantmukai.com?metier=web' class='btn btn-xs btn-custom' style='margin-top:10px;'>
-                Vous cherchez mon expe&eacute;rience en d&eacute;veloppement Web? &raquo;
-                </a>";  
-                }
-                ?>
+            <?php include 'titre_fr.html'; ?>
             <!-- //.page-scroll -->
-        </div>
-        <!-- //.hero-text -->
+			<!-- //.hero-text -->
     </div>
     <!-- //.hero-body -->
 </header>
