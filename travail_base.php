6cb9816e2fa7d284518f4862f6635d3ef9927c36
Kiyoshi Mukai <hello@kiyoshimukai.com>
2017-02-14 20:36:29 -0500

    desplacimento a company_id y no nombre de las empresas

--- travail_base.php
+++ travail_base.php
@@ -6,23 +6,23 @@ $resultatPositionsWeb = $lien->query($requetePositionsWeb);
 while($rang_positionsweb = $resultatPositionsWeb->fetch_assoc())
 {
 $i++; //Ceci ajoute 1 au numéro de "work" pour les accordéons de travail
-$a++; //Ceci ajoute 1 pour changer la gamme de déscriptions
+$a = $a + 1000; //Ceci ajoute 1 pour changer la gamme de déscriptions
 // Emplois pour WEB
-if ($a == 1 && $metier_actuel == "WEB") {
-    $descriptionsWeb = 1000;
-} elseif ($a == 2 && $metier_actuel == "WEB") {
-    $descriptionsWeb = 2000;
-} else if ($a ==3 && $metier_actuel == "WEB") {
-    $descriptionsWeb = 3000;
-}
+//if ($a == 1000 && $metier_actuel == "WEB") {
+//    $descriptionsWeb = 1000;
+//} elseif ($a == 2000 && $metier_actuel == "WEB") {
+//    $descriptionsWeb = 2000;
+//} else if ($a ==3000 && $metier_actuel == "WEB") {
+//    $descriptionsWeb = 3000;
+//}
 // Emplois pour COM
-if ($a == 1 && $metier_actuel == "COM") {
-    $descriptionsWeb = 1000;
-} elseif ($a == 2 && $metier_actuel == "COM") {
-    $descriptionsWeb = 2000;
-} else if ($a == 3 && $metier_actuel == "COM") {
-    $descriptionsWeb = 3000;
-}
+//if ($a == 1000 && $metier_actuel == "COM") {
+//    $descriptionsWeb = 1000;
+//} elseif ($a == 2000 && $metier_actuel == "COM") {
+//    $descriptionsWeb = 2000;
+//} else if ($a == 3000 && $metier_actuel == "COM") {
+//    $descriptionsWeb = 3000;
+//}
 echo "<div class='resume-item'>";
     echo "<div class='resume-year'>";
         if (is_null($rang_positionsweb['end_date'])) {
@@ -52,7 +52,7 @@ echo "<div class='resume-item'>";
                 echo "<p>
                     {$respTravail}
                     <br>";
-                    $requeteDesc = "SELECT job FROM descriptions WHERE language='{$lang_actuelle}' AND type = '{$metier_actuel}' AND company_id = ".$descriptionsWeb." ORDER BY id;";
+                    $requeteDesc = "SELECT job FROM descriptions WHERE language='{$lang_actuelle}' AND type = '{$metier_actuel}' AND company_id = ".$a." ORDER BY id;";
                     $resultatDesc = $lien->query($requeteDesc);
                     echo "<ul>";
                     while($rang_desc = $resultatDesc->fetch_assoc())
