20e73a54a4fbdf17d64d54366d56f7388d81c2c9
grantmukai <grantmukai@users.noreply.github.com>
2017-03-17 15:01:08 -0400

    Pb

--- sections/es-PR/hero.php
+++ sections/es-PR/hero.php
@@ -3,7 +3,7 @@
     <div class="hero-body">
         <div class="hero-text text-center">
             <h1>
-                Hola, mi nombre es Grant Mukai<br />Hello, my name is Grant Mukai<span class="blinker"></span>
+                Hola, mi nombre es Grant Pérez<br />Hello, my name is Grant Baker<span class="blinker"></span>
             </h1>
             <?php include "titre_es.html"; ?>
             <!-- //.page-scroll -->
