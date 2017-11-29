70384d6f2d4babe4dcb61c647035a5010e58bbc7
Kiyoshi Mukai <hello@kiyoshimukai.com>
2017-11-29 18:03:55 -0500

    nav links added in US and CAN versions

--- index.php
+++ index.php
@@ -82,9 +82,7 @@ require 'sections/'.$lang_actuelle.'/tete.php';
 require 'sections/'.$lang_actuelle.'/hero.php';
 require 'sections/'.$lang_actuelle.'/navigation.php';
 require 'sections/'.$lang_actuelle.'/profil.php';
-if ($lang_actuelle == "en-US" || $lang_actuelle == "en-CA"){
-    require 'sections/'.$lang_actuelle.'/portfolio.php';
-}
+require 'sections/'.$lang_actuelle.'/portfolio.php';
 require 'sections/'.$lang_actuelle.'/services.php';
 require 'sections/'.$lang_actuelle.'/cv.php';
 //require 'sections/'.$lang_actuelle.'/competences.php';
