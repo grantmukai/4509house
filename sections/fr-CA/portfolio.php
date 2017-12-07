9660d134271589e99f722d23d1cfc7484be0d382
Kiyoshi Mukai <hello@kiyoshimukai.com>
2017-12-06 20:43:17 -0500

    structures portfolios français

--- sections/fr-CA/portfolio.php
+++ sections/fr-CA/portfolio.php
@@ -1,220 +1,100 @@
-<!-- PORTFOLIO SECTION START --
+<!-- PORTFOLIO SECTION START -->
 <section id="portfolio" class="section">
     <div class="container-fluid section-wrapper">
         <div class="section-content">
             <div class="row">
                 <div class="col-md-12 text-center">
-                    <h2 class="section-title">My Latest Work</h2>
+                    <h2 class="section-title">Mon Travail</h2>
 
-                    <p class="section-subtitle">I'm looking to expand my portfolio while I'm on top and while I'm young.</p>
+                    <p class="section-subtitle">Je cherche à agrandir mon portfolio pendant que je suis jeune.</p>
 
                     <span class="divider center"></span>
                 </div>
-                <!-- //.col-md-12 --
+                <!-- //.col-md-12 -->
             </div>
-            <!-- //.row --
+            <!-- //.row -->
             
-            <!-- PORTFOLIO START
+            <!-- PORTFOLIO START -->
             <div class="portfolio">
                 <div class="row">
-                    <div class="item col-sm-6 col-md-3">
+                    <div class="item col-sm-4 col-md-4">
                         <div class="project-wrapper">
                             <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-1.jpg" class="zoom" title="Buzz Lightyear">
+                                <a href="assets/images/portfolio/project-image-1.jpg" class="zoom" title="Gares Ferroviaires">
                                     <i class="icon-Full-Screen"></i>
                                 </a>
 
-                                <a href="https://www.behance.net/gallery/25963987/Buzz-Lightyear" target="_blank" class="external-link">
+                                <a href="http://www.grantmukai.com/portfolio/gare-contemporaine/" target="_blank" class="external-link">
                                     <i class="icon-Link"></i>
                                 </a>
                             </div>
-                            <!-- //.project-link --
+                            <!-- //.project-link -->
 
                             <div class="project-title">
-                                <h4>Buzz Lightyear</h4>
+                                <h4>Gares Ferroviaires</h4>
                             </div>
-                            <!-- //.project-title --
+                            <!-- //.project-title -->
 
                             <img src="assets/images/portfolio/project-image-1.jpg" alt="" class="img-responsive"/>
                         </div>
-                        <!-- //.project-wrapper --
+                        <!-- //.project-wrapper -->
                     </div>
-                    <!-- //.item --
+                    <!-- //.item -->
 
-                    <div class="item col-sm-6 col-md-3">
+                    <div class="item col-sm-4 col-md-4">
                         <div class="project-wrapper">
                             <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-2.jpg" class="zoom" title="Robosam 3">
+                                <a href="assets/images/portfolio/project-image-2.jpg" class="zoom" title="Rpublic.tv">
                                     <i class="icon-Full-Screen"></i>
                                 </a>
 
-                                <a href="https://www.behance.net/gallery/25344153/Robosam-3" target="_blank" class="external-link">
+                                <a href="http://www.grantmukai.com/portfolio/rpublic/" target="_blank" class="external-link">
                                     <i class="icon-Link"></i>
                                 </a>
                             </div>
-                            <!-- //.project-link --
+                            <!-- //.project-link -->
 
                             <div class="project-title">
-                                <h4>Robosam 3</h4>
+                                <h4>Rpublic.tv</h4>
                             </div>
-                            <!-- //.project-title --
+                            <!-- //.project-title -->
 
                             <img src="assets/images/portfolio/project-image-2.jpg" alt="" class="img-responsive"/>
                         </div>
-                        <!-- //.project-wrapper --
+                        <!-- //.project-wrapper -->
                     </div>
-                    <!-- //.item --
+                    <!-- //.item -->
 
-                    <div class="item col-sm-6 col-md-3">
+                    <div class="item col-sm-4 col-md-4">
                         <div class="project-wrapper">
                             <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-3.jpg" class="zoom" title="Coke Man and Dog">
+                                <a href="assets/images/portfolio/project-image-3.jpg" class="zoom" title="Français en Amérique">
                                     <i class="icon-Full-Screen"></i>
                                 </a>
 
-                                <a href="https://www.behance.net/gallery/25821381/Coke-Man-and-Dog" target="_blank" class="external-link">
+                                <a href="http://www.grantmukai.com/portfolio/francais_amerique/" target="_blank" class="external-link">
                                     <i class="icon-Link"></i>
                                 </a>
                             </div>
-                            <!-- //.project-link --
+                            <!-- //.project-link -->
 
                             <div class="project-title">
-                                <h4>Coke Man and Dog</h4>
+                                <h4>Français en Amérique</h4>
                             </div>
-                            <!-- //.project-title --
+                            <!-- //.project-title -->
 
                             <img src="assets/images/portfolio/project-image-3.jpg" alt="" class="img-responsive"/>
                         </div>
-                        <!-- //.project-wrapper --
+                        <!-- //.project-wrapper -->
                     </div>
-                    <!-- //.item --
-
-                    <div class="item col-sm-6 col-md-3">
-                        <div class="project-wrapper">
-                            <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-4.jpg" class="zoom" title="Speed Projects">
-                                    <i class="icon-Full-Screen"></i>
-                                </a>
-
-                                <a href="https://www.behance.net/gallery/20195929/Speed-Projects" target="_blank" class="external-link">
-                                    <i class="icon-Link"></i>
-                                </a>
-                            </div>
-                            <!-- //.project-link --
-
-                            <div class="project-title">
-                                <h4>Speed Projects</h4>
-                            </div>
-                            <!-- //.project-title --
-
-                            <img src="assets/images/portfolio/project-image-4.jpg" alt="" class="img-responsive"/>
-                        </div>
-                        <!-- //.project-wrapper --
-                    </div>
-                    <!-- //.item --
-
-                    <div class="item col-sm-6 col-md-3">
-                        <div class="project-wrapper">
-                            <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-5.jpg" class="zoom" title="Wooden Rhinoceros Beetle">
-                                    <i class="icon-Full-Screen"></i>
-                                </a>
-
-                                <a href="https://www.behance.net/gallery/26043281/Wooden-Rhinoceros-Beetle" target="_blank" class="external-link">
-                                    <i class="icon-Link"></i>
-                                </a>
-                            </div>
-                            <!-- //.project-link --
-
-                            <div class="project-title">
-                                <h4>Wooden Rhinoceros Beetle</h4>
-                            </div>
-                            <!-- //.project-title --
-
-                            <img src="assets/images/portfolio/project-image-5.jpg" alt="" class="img-responsive"/>
-                        </div>
-                        <!-- //.project-wrapper --
-                    </div>
-                    <!-- //.item --
-
-                    <div class="item col-sm-6 col-md-3">
-                        <div class="project-wrapper">
-                            <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-6.jpg" class="zoom" title="The First Encounter">
-                                    <i class="icon-Full-Screen"></i>
-                                </a>
-
-                                <a href="https://www.behance.net/gallery/25505317/The-first-Encounter" target="_blank" class="external-link">
-                                    <i class="icon-Link"></i>
-                                </a>
-                            </div>
-                            <!-- //.project-link --
-
-                            <div class="project-title">
-                                <h4>The First Encounter</h4>
-                            </div>
-                            <!-- //.project-title --
-
-                            <img src="assets/images/portfolio/project-image-6.jpg" alt="" class="img-responsive"/>
-                        </div>
-                        <!-- //.project-wrapper --
-                    </div>
-                    <!-- //.item --
-
-                    <div class="item col-sm-6 col-md-3">
-                        <div class="project-wrapper">
-                            <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-7.jpg" class="zoom" title="Dead Pool">
-                                    <i class="icon-Full-Screen"></i>
-                                </a>
-
-                                <a href="https://www.behance.net/gallery/25763797/DEADPOOL" target="_blank" class="external-link">
-                                    <i class="icon-Link"></i>
-                                </a>
-                            </div>
-                            <!-- //.project-link --
-
-                            <div class="project-title">
-                                <h4>Dead Pool</h4>
-                            </div>
-                            <!-- //.project-title --
-
-                            <img src="assets/images/portfolio/project-image-7.jpg" alt="" class="img-responsive"/>
-                        </div>
-                        <!-- //.project-wrapper --
-                    </div>
-                    <!-- //.item --
-
-                    <div class="item col-sm-6 col-md-3">
-                        <div class="project-wrapper">
-                            <div class="project-link">
-                                <a href="assets/images/portfolio/project-image-8.jpg" class="zoom" title="Rebus II">
-                                    <i class="icon-Full-Screen"></i>
-                                </a>
-
-                                <a href="https://www.behance.net/gallery/19401293/Rebus-2" target="_blank" class="external-link">
-                                    <i class="icon-Link"></i>
-                                </a>
-                            </div>
-                            <!-- //.project-link --
-
-                            <div class="project-title">
-                                <h4>Rebus II</h4>
-                            </div>
-                            <!-- //.project-title --
-
-                            <img src="assets/images/portfolio/project-image-8.jpg" alt="" class="img-responsive"/>
-                        </div>
-                        <!-- //.project-wrapper --
-                    </div>
-                    <!-- //.item --
+                    <!-- //.item -->
                 </div>
-                <!-- //.row --
+                <!-- //.row -->
             </div>
-            <!-- PORTFOLIO END --
+            <!-- PORTFOLIO END -->
         </div>
         <!-- //.section-content -->
     </div
-    <!-- //.container-fluid --
+    <!-- //.container-fluid -->
 </section>
 <!-- //PORTFOLIO SECTION END -->
