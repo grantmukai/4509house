f4a63561990e18d3aebf697453fda549dd562b40
Kiyoshi Mukai <bonjour@kiyoshimukai.com>
2018-11-14 22:44:32 -0500

    profil CH

--- sections/fr-CH/profil.php
+++ sections/fr-CH/profil.php
@@ -43,10 +43,6 @@
                                 </p>
 								
                                 <p class="text-grey">
-                                    Avant mon changement de carrière du journalisme aux communications numériques, j'étais réalisateur adjoint d'actualité chez <a href='http://www.wellesleymedia.org'>Wellesley Public Media</a> dans le nord-est du pays, où j'ai produit des court métrages et documentaires pour une chaîne de télévision.
-                                </p>
-								
-                                <p class="text-grey">
                                     Je détiens un diplôme de premier cycle en journalisme télévisé à <a href='http://www.bu.edu/com'>l'École de communication</a> à l'Université de Boston avec une spécialisation en langue et littérature française.
                                 </p>
 								
