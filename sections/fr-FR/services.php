837859466dfbe06ddd9177147116fd548cb891ac
Kiyoshi Mukai <bonjour@kiyoshimukai.com>
2019-01-31 19:26:26 -0500

    France Services

--- sections/fr-FR/services.php
+++ sections/fr-FR/services.php
@@ -7,7 +7,7 @@
                     <h2 class="section-title">Ce Que Je Fais</h2>
 
                     <p class="section-subtitle">
-                        J'aime ce que je fais. Je suis fier de ce que je fais. Et je ne peux pas faire juste la moitié, trois-quarts, neuf-dixièmes de quelque chose. Si je vais embarquer sur un projet, j'y vais jusqu'au bout.
+                        J'aime ce que je fais. Je suis fier de ce que je fais. Et je ne peux pas faire juste la moitié, trois-quarts, neuf-dixièmes de quelque chose. Si j'embarquerai sur un projet, j'y irai jusqu'au bout.
                     </p>
 
                     <span class="divider center"></span>
@@ -40,7 +40,7 @@
                             <h4>2. Multimédia</h4>
 
                             <p class="text-grey">
-                                Dans mon ancienne vie, je travaillais en tant qu'animateur à la télé et monteur des vidéos. J'ai realisé mes propres reportages du début jusqu'à la fin en produisant mes propres effets spéciaux.
+                                Aux postes précedents, je travaillais en tant qu'animateur à la télé et monteur des vidéos. J'ai realisé mes propres reportages du début jusqu'à la fin en produisant mes propres effets spéciaux.
                             </p>
                         </div>
                         <!-- //.inner-content -->
@@ -54,7 +54,7 @@
                             <h4>3. Courrier Électronique</h4>
 
                             <p class="text-grey">
-                                En comprenant que le design de courrier électronique n'est pas pareil que celui du Web, je suis compétent avec le code &laquo;obsolète&raquo; du HTML et du CSS. De plus, j'ai une bonne idée de ce qu'Outlook accepte et n'accepte pas.
+                                En comprenant que la concéption de courriel n'est pas pareille que celle du Web, je sais très bien écrire les bulletins de marketing. Je sais comment tout organiser pour que les informations pertinentes soient affichées correctement.
                             </p>
                         </div>
                         <!-- //.inner-content -->
@@ -65,10 +65,10 @@
                         <div class="inner-content">
                             <i class="icon-Code-Window colored-text"></i>
 
-                            <h4>4. Code Propre</h4>
+                            <h4>4. Logiciels de Gestion de Contenu</h4>
 
                             <p class="text-grey">
-                                Je suis sérieux en écrivant seulement du code HTML5, CSS3 et PHP7 le plus propre et à jour. Mon code est correctement dentélé et il suit les standards établis par le W3C.
+                                Après avoir travaillé avec toute une gamme de logiciels de gestion de contenu tels que WordPress, Drupal et DotNetNuke, je sais bien organiser et gérer l'apparence front-end ainsi que les bases de données qui font tourner le site.
                             </p>
                         </div>
                         <!-- //.inner-content -->
@@ -79,10 +79,10 @@
                         <div class="inner-content">
                             <i class="icon-Cursor-Click2 colored-text"></i>
 
-                            <h4>5. Contenu Web</h4>
+                            <h4>5. Stratégie Numérique</h4>
 
                             <p class="text-grey">
-                                Je gère du contenu sur le Web à l'aide de plusieurs systèmes de gestion de contenu. Les logiciels que j'emploie en ce moment sont WordPress et DotNetNuke, mais j'ai utilisé aussi Drupal.
+                                La stratégie numérique est au cœur de mes responsabilités à Project HOPE. Mon but est de concevoir une stratégie qui englobe nos communications, nos efforts de collecte de fond et nos initiatives de marketing pour présenter une direction unie pour notre présence sur le Web.
                             </p>
                         </div>
                         <!-- //.inner-content -->
@@ -96,7 +96,7 @@
                             <h4>6. Gestion de projet</h4>
 
                             <p class="text-grey">
-                                Aujourd'hui, je suis en train de gérer la transition du site Web à Project HOPE. Ceci est un grand défi parce qu'il faut que je garde toutes nos équipes sur la même pages en prenant en compte leurs visions et commentaires.
+                                Aujourd'hui, j'ai géré la transition du site Web à Project HOPE. Ceci est un grand défi parce qu'il faut que je garde toutes nos équipes sur la même page en prenant en compte leurs visions et commentaires.
                             </p>
                         </div>
                         <!-- //.inner-content -->
