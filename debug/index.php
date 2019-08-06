5eedeb598032d48b18d95986974a3e7305d3caa2
Kiyoshi Mukai <bonjour@kiyoshimukai.com>
2019-08-06 19:48:57 -0400

    premier commit pour debug

---
+++ debug/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="debug.php" method="post">
+Langue et pays : <input type="text" name="languepays"><br>
+Métier: <input type="text" name="metier"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
