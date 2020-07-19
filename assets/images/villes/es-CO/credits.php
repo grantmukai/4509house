3e76c30834ef5784e301dc0e1b0a5c439811f136
Kiyoshi Mukai <bonjour@kiyoshimukai.com>
2020-07-18 20:47:38 -0400

    Fotos de Colombia

---
+++ assets/images/villes/es-CO/credits.php
@@ -0,0 +1,7 @@
+<?php
+$creditsImages = array(
+	array(0,"Bogota, Colombia","Bogot&aacute;, D.C.","Pedro Szekely","pedrosz"),
+	array(1,"Medellín","Medall&iacute;n, Antioquia","*Iván Erre Jota*","15072398@N00"),
+	array(2,"Cali, Colombia","Santiago de Cali, Valle del Cauca","tacowitte","inyucho"),
+);
+?>
