e1ac3c7bf1b3a091cc1f020eeb56df7aea7001a5
Kiyoshi Mukai <bonjour@kiyoshimukai.com>
2020-09-25 15:20:35 -0400

    Colombia agregada al menu

--- pays.php
+++ pays.php
@@ -1,10 +1,11 @@
   <ul class="dropdown-menu" role="menu">
     	<li><a href="http://www.grantmukai.com?lang=fr-CA"><img src="/assets/images/drapeaux/qc24.png" alt="" />&nbsp;Qu&eacute;bec</a></li>
-		<li><a href="http://www.grantmukai.com?lang=en-US"><img src="/assets/images/drapeaux/us24.png" alt="" />&nbsp;United States</a></li>
-    	<li><a href="http://www.grantmukai.com?lang=en-CA"><img src="/assets/images/drapeaux/ca24.png" alt="" />&nbsp;Canada</a></li>
+		  <li><a href="http://www.grantmukai.com?lang=en-CA"><img src="/assets/images/drapeaux/ca24.png" alt="" />&nbsp;Canada</a></li>
+      <li><a href="http://www.grantmukai.com?lang=en-US"><img src="/assets/images/drapeaux/us24.png" alt="" />&nbsp;United States</a></li>
+    	<li><a href="http://www.grantmukai.com?lang=es-PR"><img src="/assets/images/drapeaux/pr24.png" alt="" />&nbsp;Puerto Rico</a></li>
     	<li><a href="http://www.grantmukai.com?lang=es-MX"><img src="/assets/images/drapeaux/mx24.png" alt="" />&nbsp;M&eacute;xico</a></li>
     	<li><a href="http://www.grantmukai.com?lang=fr-FR"><img src="/assets/images/drapeaux/fr24.png" alt="" />&nbsp;France</a></li>
-    	<li><a href="http://www.grantmukai.com?lang=es-PR"><img src="/assets/images/drapeaux/pr24.png" alt="" />&nbsp;Puerto Rico</a></li>
     	<li><a href="http://www.grantmukai.com?lang=fr-CH"><img src="/assets/images/drapeaux/ch24.png" alt="" />&nbsp;Suisse</a></li>
     	<li><a href="http://www.grantmukai.com?lang=fr-BE"><img src="/assets/images/drapeaux/be24.png" alt="" />&nbsp;Belgique</a></li>
+      <li><a href="http://www.grantmukai.com?lang=es-CO"><img src="/assets/images/drapeaux/co24.png" alt="" />&nbsp;Colombia</a></li>
   </ul>
